9ad62d892280ec0a22b7aaafe83f1bf201b3d460
NOSDuco <nosduco@gmail.com>
2015-07-24 14:32:27 -0400

    Working SuperRecyclerView

--- app/src/main/java/me/tonyduco/tuhi/activity/HomeFragment.java
+++ app/src/main/java/me/tonyduco/tuhi/activity/HomeFragment.java
@@ -3,11 +3,16 @@ package me.tonyduco.tuhi.activity;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.malinskiy.superrecyclerview.OnMoreListener;
+import com.malinskiy.superrecyclerview.SuperRecyclerView;
+
 import me.tonyduco.tuhi.R;
 import me.tonyduco.tuhi.adapter.NoteAdapter;
 import me.tonyduco.tuhi.decoration.DividerItemDecoration;
@@ -15,7 +20,7 @@ import me.tonyduco.tuhi.decoration.DividerItemDecoration;
 public class HomeFragment extends Fragment {
 
 
-    private RecyclerView mRecyclerView;
+    private SuperRecyclerView mRecyclerView;
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
 
@@ -36,10 +41,9 @@ public class HomeFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.fragment_home, container, false);
 
 
-
         // Inflate the layout for this fragment
-        mRecyclerView = (RecyclerView)rootView.findViewById(R.id.note_view);
-        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView = (SuperRecyclerView) rootView.findViewById(R.id.note_view);
+       // mRecyclerView.setHasFixedSize(true);
 
         mLayoutManager = new LinearLayoutManager(getActivity());
         mRecyclerView.setLayoutManager(mLayoutManager);
@@ -48,16 +52,29 @@ public class HomeFragment extends Fragment {
         mAdapter = new NoteAdapter(getActivity());
         mRecyclerView.setAdapter(mAdapter);
 
-        return rootView;
-    }
+        mRecyclerView.setRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                mAdapter.notifyDataSetChanged();
+            }});
 
-    @Override
-    public void onAttach(Activity activity) {
-        super.onAttach(activity);
-    }
+        mRecyclerView.setupMoreListener(new OnMoreListener() {
+            @Override
+            public void onMoreAsked(int numberOfItems, int numberBeforeMore, int currentItemPos) {
+                // Fetch more from Api or DB
+            }
+        }, 10);
 
-    @Override
-    public void onDetach() {
-        super.onDetach();
+            return rootView;
+        }
+
+        @Override
+        public void onAttach (Activity activity){
+            super.onAttach(activity);
+        }
+
+        @Override
+        public void onDetach () {
+            super.onDetach();
+        }
     }
-}
--- app/src/main/java/me/tonyduco/tuhi/adapter/NoteAdapter.java
+++ app/src/main/java/me/tonyduco/tuhi/adapter/NoteAdapter.java
@@ -8,6 +8,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.malinskiy.superrecyclerview.OnMoreListener;
+
 import java.util.List;
 
 import me.tonyduco.tuhi.R;
