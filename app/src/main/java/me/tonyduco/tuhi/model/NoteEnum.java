3ee4f518fda57a142a9f5436305f79da114ed976
nosduco <nosduco@gmail.com>
2015-08-07 22:18:52 -0400

    Created NoteEnum

---
+++ app/src/main/java/me/tonyduco/tuhi/model/NoteEnum.java
@@ -0,0 +1,18 @@
+package me.tonyduco.tuhi.model;
+
+public enum NoteEnum {
+    PLAINTEXT(0),
+    DELETED(-1),
+    PERMANENTLY_DELETED(-2);
+
+    private final int type;
+
+    NoteEnum(int type){
+        this.type = type;
+    }
+
+    public int getType(){
+        return this.type;
+    }
+
+}
